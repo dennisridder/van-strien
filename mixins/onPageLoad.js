b9988610e2b82b2d16ec4677f79b9e97320f46c7
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-17 10:30:16 +0200

    added timeout on pageload mixins

--- mixins/onPageLoad.js
+++ mixins/onPageLoad.js
@@ -12,97 +12,104 @@ export default {
   },
   methods: {
     scrollSlowDoor() {
-      var targets = document.querySelectorAll(".scrollSlow-Door")
-      targets.forEach((el) => {
-        gsap.set(el, {
-          yPercent: -10
+      // prettier-ignore
+      setTimeout(function() {
+        var targets = document.querySelectorAll(".scrollSlow-Door")
+        targets.forEach((el) => {
+          gsap.set(el, {
+            yPercent: -10
+          })
+          gsap.to(el, {
+            yPercent: 10,
+            ease: "none",
+            scrollTrigger: {
+              trigger: el,
+              scrub: true,
+              start: "top bottom",
+              end: "bottom top"
+            }
+          })
         })
-        gsap.to(el, {
-          yPercent: 10,
-          ease: "none",
-          scrollTrigger: {
-            trigger: el,
-            scrub: true,
-            start: "top bottom",
-            end: "bottom top"
-          }
-        })
-      })
+      }, 200)
     },
     scrollSlowLanding() {
-      var targets = document.querySelectorAll(".scrollSlow-Landing")
-      targets.forEach((el) => {
-        gsap.to(el, {
-          yPercent: 10,
-          ease: "none",
-          scrollTrigger: {
-            trigger: el,
-            scrub: true,
-            start: "top top",
-            end: "bottom top"
-          }
+      // prettier-ignore
+      setTimeout(function() {
+        var targets = document.querySelectorAll(".scrollSlow-Landing")
+        targets.forEach((el) => {
+          gsap.to(el, {
+            yPercent: 10,
+            ease: "none",
+            scrollTrigger: {
+              trigger: el,
+              scrub: true,
+              start: "top top",
+              end: "bottom top"
+            }
+          })
         })
-      })
+      }, 200)
     },
     horizontalScrollElement() {
       // Get page width
-      var width = window.innerWidth / 3
-      // Get el
-      var array = document.querySelectorAll(".horizontalScroll")
-      array.forEach((el) => {
-        let horizontalTimeline = gsap.timeline({
-          scrollTrigger: {
-            trigger: el,
-            scrub: 0,
-            start: "top bottom",
-            end: "bottom top",
-            ease: "none"
-          }
+      // prettier-ignore
+      setTimeout(function() {
+        var width = window.innerWidth / 3
+        // Get el
+        var array = document.querySelectorAll(".horizontalScroll")
+        array.forEach((el) => {
+          let horizontalTimeline = gsap.timeline({
+            scrollTrigger: {
+              trigger: el,
+              scrub: 0,
+              start: "top bottom",
+              end: "bottom top",
+              ease: "none"
+            }
+          })
+          horizontalTimeline.set(el, {
+            x: 0
+          })
+          horizontalTimeline.to(el, {
+            x: -width
+          })
         })
-        horizontalTimeline.set(el, {
-          x: 0
-        })
-        horizontalTimeline.to(el, {
-          x: -width
-        })
-      })
+      }, 200)
     },
     rotateElement() {
       // Get doc height
-      var body = document.body,
-        html = document.documentElement
-      var height = Math.max(
-        body.scrollHeight,
-        body.offsetHeight,
-        html.clientHeight,
-        html.scrollHeight,
-        html.offsetHeight
-      )
-      // Calc number of rotations
-      var rotations = height / (window.innerHeight * 3)
-      var container = document.querySelector("main")
-      // Scrolltrigger animation
-      var array = document.querySelectorAll(".rotate")
-      array.forEach((el) => {
-        let rotateTimeline = gsap.timeline({
-          scrollTrigger: {
-            trigger: container,
-            scrub: 0,
-            start: "top top",
-            end: "bottom bottom",
+      // prettier-ignore
+      setTimeout(function() {
+        var body = document.body,
+          html = document.documentElement
+        var height = Math.max(
+          body.scrollHeight,
+          body.offsetHeight,
+          html.clientHeight,
+          html.scrollHeight,
+          html.offsetHeight
+        )
+        // Calc number of rotations
+        var rotations = height / (window.innerHeight * 3)
+        var container = document.querySelector("main")
+        // Scrolltrigger animation
+        var array = document.querySelectorAll(".rotate")
+        array.forEach((el) => {
+          let rotateTimeline = gsap.timeline({
+            scrollTrigger: {
+              trigger: container,
+              scrub: 0,
+              start: "top top",
+              end: "bottom bottom",
+              ease: "none"
+            }
+          })
+          rotateTimeline.to(el, {
+            rotation: 360 * rotations,
             ease: "none"
-          }
+          })
         })
-        // rotateTimeline.set(el, {
-        //   rotation: 0,
-        //   duration: 0,
-        //   ease: "none"
-        // })
-        rotateTimeline.to(el, {
-          rotation: 360 * rotations,
-          ease: "none"
-        })
-      })
+      }, 200)
     }
   }
 }
