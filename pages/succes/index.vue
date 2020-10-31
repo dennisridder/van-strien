c73b90261a47978508c720a6de021ee7abcc8644
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-31 14:18:49 +0100

    error + succes pages

--- pages/succes/index.vue
+++ pages/succes/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="section-Wrapper section-Page">
-    <section class="section-Error">
-      <div class="section-Error_Content">
+    <section class="section-Succes">
+      <div class="section-Succes_Content">
         <p>
-          Oops! Deze pagina bestaat niet.<br />
-          Ga naar <nuxt-link to="/">home</nuxt-link>,
+          Succes! Bedankt voor je bericht.<br />
+          Ga terug naar <nuxt-link to="/">home</nuxt-link>,
           <nuxt-link to="/moonlab">Moonlab</nuxt-link> of lees mijn
           <nuxt-link to="/herinneringen">herinneringen</nuxt-link>.
         </p>
@@ -17,7 +17,7 @@
 @import '~/assets/styles/variables.sass'
 
 .section
-  &-Error
+  &-Succes
     min-height: 100vh
     width: 100%
     display: flex
