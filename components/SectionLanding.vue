e2e357fb7bf4a284bed5bef32ceb49a3e42c6ec2
Dries Bos <driesbos@MBP-van-Dries.home>
2020-11-09 09:37:21 +0100

    image optimise landingSection

--- components/SectionLanding.vue
+++ components/SectionLanding.vue
@@ -10,14 +10,17 @@
     </div>
     <!-- prettier-ignore -->
     <img
-      v-lazy="`${transformImage(blok.background_image, '1440x0')}`"
+      v-lazy="`${transformImage(blok.background_image, '1680x0/filters:format(jpg):quality(40)')}`"
       :data-srcset="
-        `${transformImage(blok.background_image, '400x0')} 400w, 
-          ${transformImage(blok.background_image, '800x0')} 800w, 
-          ${transformImage(blok.background_image, '1200x0')} 1200w,
-          ${transformImage(blok.background_image, '1600x0')} 1600w,
-          ${transformImage(blok.background_image, '2000x0')} 2000w`"
+        `${transformImage(blok.background_image, '2880x0/filters:format(jpg):quality(40)')} 2880w,
+        ${transformImage(blok.background_image, '2560x0/filters:format(jpg):quality(40)')} 2560w, 
+        ${transformImage(blok.background_image, '1920x0/filters:format(jpg):quality(40)')} 1920w, 
+        ${transformImage(blok.background_image, '1680x0/filters:format(jpg):quality(40)')} 1680w, 
+        ${transformImage(blok.background_image, '1280x0/filters:format(jpg):quality(40)')} 1280w,
+        ${transformImage(blok.background_image, '1024x0/filters:format(jpg):quality(40)')} 1024w, 
+        ${transformImage(blok.background_image, '768x0/filters:format(jpg):quality(40)')} 768w`"
       class="lazy scrollSlow-Landing"
+      sizes="100w"
       :alt="blok.background_image" />
   </section>
 </template>
